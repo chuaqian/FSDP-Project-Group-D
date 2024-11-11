2b93b725c9ae727b70b4ec3148ebdcca5cffb8fa
Dexter Wong <dexterwong0724@gmail.com>
2024-11-11 22:23:30 +0800

    ammended file route

--- src/Shortcuts.tsx
+++ src/Shortcuts.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import OCBCLogo from './images/OCBC Logo.png';
+import OCBCLogo from './images/OCBC-Logo.png';
 import { db, collection, addDoc, getDocs } from './firebaseConfig';
 import WatsonChat from './WatsonChat';
 
